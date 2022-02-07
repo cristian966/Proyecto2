f20bd02f390fa4c2d5c05638c353de2207b0ea01
Cristian <cris96_ture@hotmail.com>
2022-02-07 03:45:33 +0100

    Pagina del foro principal, mostrando las categorias

--- foro/foro_principal.php
+++ foro/foro_principal.php
@@ -1,3 +1,14 @@
+<?php
+ include_once("../php/conexion.php");
+ session_start();
+
+ if (isset($_SESSION['nombredelusuario'])) {
+     $usuarioingresado = $_SESSION['nombredelusuario'];
+ }else{
+     header('location: ../index.php');
+ }
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -25,34 +36,51 @@
         </div>
     </div>
     <div class="login">
+
     <?php
-        session_start();
+        // session_start();
         if (isset($_SESSION['nombredelusuario'])) {
             echo " <a href='../usuarios/registrado/registrado.php' class='enlacesIniSesion'><h3>Perfil del usuario</h3></a> <br>";
         }else {
             echo "<a href='../login/login.php' class='enlacesIniSesion'><h3>Iniciar sesión / Registrarse</h3></a>";
         }
     ?>
-        
+    
     </div>
 
     <h1>Foro</h1>
 
     <div>
-        <h2>Categorías</h2>
-        <div>
-            <div>
-                <h3>General</h3>
-            </div>
-            <div>
-                <h3>Juegos</h3>
-            </div>
-            <div>
-                <h3>Noticias</h3>
-            </div>
-            <div>
-                <h3>Off-topic</h3>
-            </div>
+        <table>
+			<div id="barrabuscar">
+		<form method="POST">
+		<input type="submit" value="Buscar" name="btnbuscar"><input type="text" name="txtbuscar" id="cajabuscar" placeholder="&#128270;Nombre de la Categoría">
+		</form>
+		</div>
+			<tr><th colspan="5"><h1>Categorías</h1></tr>
+			<tr>
+		    <th>Nombre</th>
+			</tr>
+        <?php 
+
+if(isset($_POST['btnbuscar']))
+{
+$buscar = $_POST['txtbuscar'];
+$queryusuarios = mysqli_query($conn, "SELECT nombre_categoria FROM categorias where nombre_categoria like '".$buscar."%'");
+}
+else
+{
+$queryusuarios = mysqli_query($conn, "SELECT nombre_categoria FROM categorias ORDER BY id_categoria asc");
+}
+		$numerofila = 0;
+        while($mostrar = mysqli_fetch_array($queryusuarios)) 
+		{    $numerofila++;    
+            echo "<tr>";
+            echo "<td>".$mostrar['nombre_categoria']."</td>"; 
+                   
+}
+        ?>
+    </table>
         </div>
     </div>
 
